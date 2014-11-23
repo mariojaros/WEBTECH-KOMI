ec3c7243970340a9a13aa416db2e85bad8e792cc
mariojaros <mario.jaros@student.tuke.sk>
2014-11-23 22:55:33 +0100

    Oprava hashovania.

--- WebTech-api/src/main/java/com/jcrons/HashTool.java
+++ WebTech-api/src/main/java/com/jcrons/HashTool.java
@@ -1,22 +1,13 @@
 package com.jcrons;
 
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import com.google.common.base.Charsets;
+import com.google.common.hash.Hashing;
 
 public class HashTool {
-	
 	public String toSHA256(String password){
-		try {
-			MessageDigest digest = MessageDigest.getInstance("SHA-256");
-			digest.update(password.getBytes("UTF-8"));
-			return new String (digest.digest());
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		} catch (UnsupportedEncodingException e) {			
-			e.printStackTrace();
-		}
-		return password;
+		String hash = Hashing.sha256().hashString(password, Charsets.UTF_8).toString();
+		
+		return hash;
 		
 	}
 }
